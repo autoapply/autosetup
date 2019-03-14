e06c3805c5b495862e2730fc0295dbd4fd4fe9b6
Pascal <pascal.gruen@gmail.com>
2019-03-14 21:46:55 +0100

    Update dependencies

--- src/config.js
+++ src/config.js
@@ -79,7 +79,7 @@ const defaultConfig: Config = {
     git: {
       args: "--depth 1 --single-branch"
     },
-    image: "autoapply/autoapply:0.8.0-kubectl",
+    image: "autoapply/autoapply:0.8.3-kubectl",
     path: "."
   }
 };
